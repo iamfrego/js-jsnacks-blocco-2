584e5b2cda888622ea5a38f256720c8063206a52
iamfrego <mate_freg@tiscali.it>
2021-10-25 12:38:16 +0200

    done

--- assets/main2.js
+++ assets/main2.js
@@ -15,3 +15,18 @@ const squadre = [
         falliSubiti: 0,
     },
 ]
+
+for (let i = 0; i < squadre.length; i++) {
+
+    squadre[i].gol = getRandomIntInclusive(0, 50);
+
+    console.log(squadre[i].gol);
+
+    squadre[i].falliSubiti = getRandomIntInclusive(0, 50);
+}
+
+console.log(squadre);
+
+function getRandomIntInclusive(min, max) {
+    return Math.floor(Math.random() * (max - min + 1) + min); //The maximum is inclusive and the minimum is inclusive
+}
