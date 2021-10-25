c9438314e004fd3012aec8d6632aba091a03994f
iamfrego <mate_freg@tiscali.it>
2021-10-25 12:23:26 +0200

    sono confuso

--- assets/main.js
+++ assets/main.js
@@ -1,36 +1,37 @@
-const bike = [
-    {
-        marca: 'Pinarello',
-        peso: '4',
-    },
-    {
-        marca: 'Bianchi',
-        peso: '5',
-    },
-    {
-        marca: 'Olympia',
-        peso: '6',
-    },
-]
+// const bike = [
+//     {
+//         marca: 'Pinarello',
+//         peso: '4',
+//     },
+//     {
+//         marca: 'Bianchi',
+//         peso: '5',
+//     },
+//     {
+//         marca: 'Olympia',
+//         peso: '6',
+//     },
+// ]
 
-//console.log(bike);
+// //console.log(bike);
 
-let minName;
+// let minName;
 
-let minWeight = 100;
+// let minWeight = 100;
 
 
-for (let i = 0; i < bike.length; i++) {
+// for (let i = 0; i < bike.length; i++) {
 
-    //console.log(bike[i].peso);
+//     //console.log(bike[i].peso);
 
-    if (bike[i].peso < minWeight) {
-        minWeight = bike[i].peso;
-        minName = bike[i].marca;
-    }
-    //console.log(minWeight);
-}
+//     if (bike[i].peso < minWeight) {
+//         minWeight = bike[i].peso;
+//         minName = bike[i].marca;
+//     }
+//     //console.log(minWeight);
+// }
 
-console.log(minWeight);
+// console.log(minWeight);
 
-document.getElementById("lower_weight").innerHTML = `Marca più leggera: ${minName} <br> Peso ${minWeight} kg`;
+// document.getElementById("lower_weight").innerHTML = `Marca più leggera: ${minName} <br> Peso ${minWeight} kg`;
+
---
+++ assets/main2.js
@@ -0,0 +1,17 @@
+const squadre = [
+    {
+        nomeSquadra: 'Juve',
+        gol: 0,
+        falliSubiti: 0,
+    },
+    {
+        nomeSquadra: 'Inter',
+        gol: 0,
+        falliSubiti: 0,
+    },
+    {
+        nomeSquadra: 'Milan',
+        gol: 0,
+        falliSubiti: 0,
+    },
+]
