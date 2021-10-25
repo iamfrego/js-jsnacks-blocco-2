7a5a403da7ffa23c29e905d2e993067e95e12280
iamfrego <mate_freg@tiscali.it>
2021-10-25 11:47:03 +0200

    Dom Complete

--- assets/main.js
+++ assets/main.js
@@ -15,7 +15,10 @@ const bike = [
 
 //console.log(bike);
 
+let minName;
+
 let minWeight = 100;
+
 
 for (let i = 0; i < bike.length; i++) {
 
@@ -23,9 +26,11 @@ for (let i = 0; i < bike.length; i++) {
 
     if (bike[i].peso < minWeight) {
         minWeight = bike[i].peso;
+        minName = bike[i].marca;
     }
     //console.log(minWeight);
 }
 
 console.log(minWeight);
 
+document.getElementById("lower_weight").innerHTML = `Marca più leggera: ${minName} <br> Peso ${minWeight} kg`;
