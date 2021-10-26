cc354360f86030360d7a1e757bbc4377c0a87183
iamfrego <mate_freg@tiscali.it>
2021-10-26 12:32:00 +0200

    es 3 con else se la somma supera il 50

--- es3/assets/js/main.js
+++ es3/assets/js/main.js
@@ -7,6 +7,8 @@ while (sum < 50) {
     console.log(sum);
     if (sum < 50) {
         list.push(n);
+    } else {
+        console.log("Somma elementi maggiore di 50")
     }
     console.log(list);
 }
